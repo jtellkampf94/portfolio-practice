43dd70087d7ba0f4bae063d0c1eb2482abee9c7b
Jonathan <jtellkampf@gmail.com>
2022-04-29 23:12:09 +0100

    Created home page component

---
+++ src/components/AnimatedLetters/index.tsx
@@ -0,0 +1,25 @@
+import './index.scss'
+
+interface AnimatedLettersProps {
+  letterClass: string
+  strArray: string[]
+  idx: number
+}
+
+const AnimatedLetters: React.FC<AnimatedLettersProps> = ({
+  letterClass,
+  strArray,
+  idx,
+}) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
+
+export default AnimatedLetters
